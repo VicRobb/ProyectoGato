be708e6fc0710f0f762f099a6088563a3f6d5855
Car1014 <carlosroo1014@gmail.com>
2023-09-30 12:38:40 -0600

    A: router

--- Gatoo/src/App.vue
+++ Gatoo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<body class="bg-black">
-    
+		<router-view />
 	</body>	
 	<h1 className="text-3xl font-bold underline">
 		
@@ -9,11 +9,15 @@
 
 	</pagina_pricipal>
 
+	<tablero>
+	</tablero>
 
 </template>
 
 <script setup>
 
 	import pagina_pricipal from './pagina_principal/pagina_principal.vue'
+	import tablero from './components/Tablero.vue'
+
 
 </script>
