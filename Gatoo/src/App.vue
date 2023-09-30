e667da90eea3455ee8ca6166eb06bb403ed0683c
VictorRobb <victorescdiaz4@gmail.com>
2023-09-30 12:49:40 -0600

    cambios peques

--- Gatoo/src/App.vue
+++ Gatoo/src/App.vue
@@ -1,15 +1,11 @@
 <template>
-	<body class="bg-black">
-    
-	</body>	
+	
 	<h1 className="text-3xl font-bold underline">
-		
+		Hello world!
 	</h1>
 	<pagina_pricipal>
 
 	</pagina_pricipal>
-
-	</body>
 </template>
 
 <script setup>
--- Gatoo/src/pagina_principal/pagina_principal.vue
+++ Gatoo/src/pagina_principal/pagina_principal.vue
@@ -1,10 +1,36 @@
 <template>
 
-   
-    <section class="text-center text-light_blue">
-        hola
+    
+    <section class="flex flex-col items-center ">
+        <div class="">
+            <img src="../assets/logo.svg" alt="no" width="100" height="100">
+        </div>
+        <div class="rounded-2xl  w-1/3  text-silver p-3 bg-semi_dark_navy" bg->
+            <div class="p-5 text-center">
+                PICK PLAYER 1'S MARK
+            </div>
+            <div class="bg-dark_navy">
+                
+                <img src="../assets/icon-x-outline.svg" alt="">
+                <img src="" alt="">
+            </div>
+            <div class="p-5 text-center">
+                REMEMBER:X GOES FIRS
+            </div>
+            
+        </div>
+
+        <div class="p-6">
+        <Button class="rounded-2xl p-4 bg-light_yellow text-dark_navy"> NEW GAME (VS CPU) </Button>
+        </div>
+        <div>
+            <BuTton class="rounded-2xl p-4 bg-light_blue text-dark_navy"> NEW GAME (VS PLAYER)</BuTton>
+        </div>
+    
+
     </section>
-    <img src="../../../assets/logo.svg" alt="no">
+
+   
 
 </template>
 
